eea480813f693d0e0f0da1c8ead1e138dda49a3a
faqih <faqihsidj@gmail.com>
2022-07-15 16:44:25 +0700

    create method delete transaction

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,105 +4,64 @@ import 'package:udemy_expense_app/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   List<Transaction> transactions;
-  TransactionList(this.transactions);
+  final Function deleteTx;
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
-        child: transactions.isEmpty
-            ? Column(
-                children: [
-                  Text(
-                    "No transactions added yet!",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Image.asset(
-                    "assets/images/waiting.png",
-                    width: 100,
-                  ),
-                ],
-              )
-            : ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FittedBox(
-                              child: Text("\$ ${transactions[index].amount}")),
-                        ),
+      height: 300,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "No transactions added yet!",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  "assets/images/waiting.png",
+                  width: 100,
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text("\$ ${transactions[index].amount}")),
                       ),
-                      title: Text(
-                        "${transactions[index].title}",
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      subtitle: Text(
-                        DateFormat.yMMMd().format(
-                          transactions[index].date ?? DateTime.now(),
-                        ),
-                      ),
-                      trailing: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.delete,
-                            color: Colors.red,
-                          )),
                     ),
-                  );
-                  // return Card(
-                  //   child: Row(
-                  //     children: [
-                  //       Container(
-                  //         margin: EdgeInsets.symmetric(
-                  //             vertical: 10, horizontal: 15),
-                  //         padding: EdgeInsets.all(10),
-                  //         decoration: BoxDecoration(
-                  //           border: Border.all(
-                  //             color: Theme.of(context).primaryColor,
-                  //             width: 2,
-                  //           ),
-                  //         ),
-                  //         child: Text(
-                  //           "\$ ${transactions[index].amount?.toStringAsFixed(2)}",
-                  //           style: Theme.of(context)
-                  //               .textTheme
-                  //               .headline6!
-                  //               .copyWith(
-                  //                   color: Theme.of(context).primaryColor),
-                  //         ),
-                  //       ),
-                  //       Column(
-                  //         crossAxisAlignment: CrossAxisAlignment.start,
-                  //         children: [
-                  //           Text(
-                  //             transactions[index].title ?? '',
-                  //             style: TextStyle(
-                  //               fontSize: 16,
-                  //               fontWeight: FontWeight.bold,
-                  //             ),
-                  //           ),
-                  //           Text(
-                  //             DateFormat.yMMMMd().format(
-                  //                 transactions[index].date ?? DateTime.now()),
-                  //             style: TextStyle(
-                  //               color: Colors.grey,
-                  //             ),
-                  //           ),
-                  //         ],
-                  //       ),
-                  //     ],
-                  //   ),
-                  // );
-                },
-                itemCount: transactions.length,
-              ));
+                    title: Text(
+                      "${transactions[index].title}",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(
+                        transactions[index].date ?? DateTime.now(),
+                      ),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () => deleteTx(transactions[index].id),
+                      icon: Icon(
+                        Icons.delete,
+                        color: Theme.of(context).errorColor,
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
   }
 }
